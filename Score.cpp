ffeb82d1115d3eb3257fbbd49dd4fb1197e739fb
cmyui <cmyuisuki@gmail.com>
2020-04-19 03:21:07 -0400

    Improve Score::getTimeFormatted()

--- Score.cpp
+++ Score.cpp
@@ -1,13 +1,18 @@
 #include "Score.h"
 
 namespace cmyui {
-    std::string Score::getTimeFormatted(int input) const {
-        if (!input) return "now";
-        int seconds = input;
-        int hours = seconds / 60.f / 60.f;
-        seconds = input % (60 * 60);
-        int minutes = seconds / 60.f;
-        seconds = input % 60;
-        return std::to_string(hours) + "h " + std::to_string(minutes) + "m " + std::to_string(seconds) + "s";
+    std::string Score::getTimeFormatted(int seconds) const {
+        if (!seconds) return "now";
+        std::string ret;
+        int days = int(seconds / 60 / 60 / 24);
+        seconds %= (60 * 60 * 24);
+        int hours = int(seconds / 60 / 60);
+        seconds %= (60 * 60);
+        int minutes = int(seconds / 60);
+        seconds %= 60;
+
+        if (days) ret += std::to_string(days) + "d ";
+        if (hours) ret += std::to_string(hours) + "h ";
+        return (ret += std::to_string(minutes) + "m " + std::to_string(seconds) + "s", ret);
     }
 }
